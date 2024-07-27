c4607f720473b42aba7810124ef71f4a0b8a77ea
Yosuke Nishiyama <141266522+Yunexiz@users.noreply.github.com>
2024-07-27 21:04:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+const apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
+const buttonContainer = document.getElementById('button-container');
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    const imageUrl = data[0].url;
-    const imageElement = document.createElement('img');
-    imageElement.src = imageUrl;
-
-    const imageContainer = document.getElementById('cat-pics');
-    imageContainer.appendChild(imageElement);
+    data.forEach((cat, index) => {
+      const button = document.createElement('button');
+      button.textContent = `Image ${index + 1}`;
+      button.addEventListener('click', () => {
+        const formData = new FormData();
+        formData.append('imageUrl', cat.url);
+        fetch('/search.html', {
+          method: 'POST',
+          body: formData
+        })
+        .then(response => {
+          window.location.href = '/search.html';
+        })
+        .catch(error => {
+          console.error('Error sending POST request:', error);
+        });
+      });
+      buttonContainer.appendChild(button);
+    });
   })
   .catch(error => {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching cat images:', error);
   });
