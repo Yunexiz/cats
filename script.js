52f49fb15f0a32319a9346605adde7ef967cc17c
Yosuke Nishiyama <141266522+Yunexiz@users.noreply.github.com>
2024-07-27 20:38:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const apiUrl = 'https://meme-api.com/gimme/catpictures';
-
-fetch(apiUrl)
+fetch('https://meme-api.com/gimme/catpictures')
   .then(response => response.json())
   .then(data => {
     const imageUrl = data.url;
-    const imageElement = document.createElement('img');
-    imageElement.src = imageUrl;
-    imageElement.alt = 'Cat picture from Reddit' ;
 
-    const imageContainer = document.getElementById('cat-pics');
-    imageContainer.appendChild(imageElement);
+    // Create a new meta tag
+    const ogImageMeta = document.createElement('meta');
+    ogImageMeta.property = 'og:image';
+    ogImageMeta.content = imageUrl;
+
+    // Find the existing og:image meta tag (if any)
+    const existingOgImage = document.querySelector('meta[property="og:image"]');
+    if (existingOgImage) {
+      existingOgImage.parentNode.replaceChild(ogImageMeta, existingOgImage);
+    } else {
+      document.head.appendChild(ogImageMeta);
+    }
   })
   .catch(error => {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching image:', error);
   });
